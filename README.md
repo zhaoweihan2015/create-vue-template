aee92d094b57b16f140b7703b4d756bee781eec8
赵玮涵 <zhaoweihan01@nuctech.com>
2025-09-02 15:34:06 +0800

    feat: add vite-plugin-compression2

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ unplugin-vue-components // 自动导入 vue 组件
 unplugin-vue-router // 约定式路由
 orval // swagger yaml 生成axios代码
 vite-plugin-vue-inspector // 点击页面元素直达源码位置
+vite-plugin-compression2 // 打包后文件压缩
 ```
 
 ### 命令行
@@ -16,5 +17,6 @@ vite-plugin-vue-inspector // 点击页面元素直达源码位置
 ```cmd
 yarn dev // 开发
 yarn swagger // 根据 swagger 生成axios代码，并prettier代码
+yarn build:compress // 带压缩的打包
 ```
 
--- vite.config.ts
+++ vite.config.ts
@@ -8,73 +8,97 @@ import VueRouter from 'unplugin-vue-router/vite';
 import AutoImport from 'unplugin-auto-import/vite';
 import Inspector from 'vite-plugin-vue-inspector';
 import Inspect from 'vite-plugin-inspect';
+import { compression } from 'vite-plugin-compression2';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '~/': `${path.resolve(__dirname, 'src')}/`,
-    },
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const enableCompression = env.VITE_ENABLE_COMPRESSION === 'true';
 
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "~/styles/element/index.scss" as *;`,
+  return {
+    resolve: {
+      alias: {
+        '~/': `${path.resolve(__dirname, 'src')}/`,
       },
     },
-  },
 
-  plugins: [
-    Vue(),
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "~/styles/element/index.scss" as *;`,
+        },
+      },
+    },
 
-    // https://github.com/posva/unplugin-vue-router
-    VueRouter({
-      extensions: ['.vue', '.md'],
-      exclude: ['**/components/**'],
-      dts: 'src/typed-router.d.ts',
-    }),
+    plugins: [
+      Vue(),
 
-    Components({
-      dirs: ['src/components', 'src/pages'],
-      extensions: ['vue', 'md'],
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      resolvers: [
-        ElementPlusResolver({
-          importStyle: 'sass',
-        }),
-      ],
-      dts: 'src/components.d.ts',
-    }),
+      // https://github.com/posva/unplugin-vue-router
+      VueRouter({
+        extensions: ['.vue', '.md'],
+        exclude: ['**/components/**'],
+        dts: 'src/typed-router.d.ts',
+      }),
 
-    AutoImport({
-      imports: [
-        'vue', // 自动导入 Vue Composition API
-        'vue-router', // 自动导入 useRouter / useRoute
-        'pinia', // 自动导入 defineStore / storeToRefs
-      ],
-      dirs: ['src/composables'], // 可选：自动导入你自己写的组合函数
-      vueTemplate: true, // 在模板中也可直接使用
-      dts: 'src/auto-imports.d.ts', // 生成类型声明文件
-    }),
+      Components({
+        dirs: ['src/components', 'src/pages'],
+        extensions: ['vue', 'md'],
+        include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+        resolvers: [
+          ElementPlusResolver({
+            importStyle: 'sass',
+          }),
+        ],
+        dts: 'src/components.d.ts',
+      }),
 
-    // https://github.com/antfu/unocss
-    // see uno.config.ts for config
-    Unocss(),
+      AutoImport({
+        imports: [
+          'vue', // 自动导入 Vue Composition API
+          'vue-router', // 自动导入 useRouter / useRoute
+          'pinia', // 自动导入 defineStore / storeToRefs
+        ],
+        dirs: ['src/composables'], // 可选：自动导入你自己写的组合函数
+        vueTemplate: true, // 在模板中也可直接使用
+        dts: 'src/auto-imports.d.ts', // 生成类型声明文件
+      }),
 
-    // A vite plugin which provides the ability that to jump to the local IDE when you click the element of browser automatically. It supports Vue2 & 3 & SSR.
-    Inspector({
-      enabled: true,
-      toggleButtonVisibility: 'always',
-      launchEditor: 'cursor',
-    }),
-    Inspect(),
-  ],
+      // https://github.com/antfu/unocss
+      // see uno.config.ts for config
+      Unocss(),
 
-  ssr: {
-    // TODO: workaround until they support native ESM
-    noExternal: ['element-plus'],
-  },
+      // A vite plugin which provides the ability that to jump to the local IDE when you click the element of browser automatically. It supports Vue2 & 3 & SSR.
+      Inspector({
+        enabled: true,
+        toggleButtonVisibility: 'always',
+        launchEditor: 'cursor', // 改成正在使用的编辑器
+      }),
+      Inspect(),
+      // 压缩
+      enableCompression
+        ? compression({
+            deleteOriginalAssets: true, // 删除原始文件
+            skipIfLargerOrEqual: true, // 如果压缩后文件大于原始文件，则不压缩
+          })
+        : null,
+    ],
+    // 服务器端渲染
+    ssr: {
+      // TODO: workaround until they support native ESM
+      noExternal: ['element-plus'],
+    },
+    // 代理
+    server: {
+      host: '0.0.0.0',
+      port: 8080,
+      proxy: {
+        '/api': {
+          target: 'http://127.0.0.1:8080',
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 });
